851ef30868ccbccc77e7a45a0f9823e3f8ef91d3
Matthew <m.bednarski@gmail.com>
2022-02-08 00:17:19 +0000

    Write query to calculate total time spent on assingments by a given cohort

---
+++ 2_queries_joins/2_total_assignment_duration_cohort.sql
@@ -0,0 +1,5 @@
+SELECT SUM(assignment_submissions.duration) AS total_duration
+FROM students 
+JOIN cohorts ON students.cohort_id = cohorts.id
+JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+WHERE cohorts.name = 'FEB12';
