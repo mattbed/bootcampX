791f02b88e0a556412ce6080b03360a778dc102d
Matthew <m.bednarski@gmail.com>
2022-02-10 05:41:10 +0000

    Query the database using Node Postgres to find some student info

---
+++ bootcamp_app/students.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+const input = process.argv.slice(2);
+const cohort = input[0];
+const limit = input[1];
+
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT students.id, students.name, cohorts.name as cohort_name
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '${cohort}%'
+LIMIT ${limit || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
